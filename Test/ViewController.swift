e082700947389d23fdd87df7da554da0246d6d7c
Pranav Joshi <joshipranav5194@gmail.com>
2017-04-06 14:11:04 +1000

    Pranav - push

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -81,7 +81,7 @@ class ViewController: UIViewController {
                 
                 FIRAuth.auth()?.currentUser?.sendEmailVerification( completion: { (error) in
                     // ...
-                    print("sent email - TESTTTTTTTTTTTT")
+                    print("sent email - Jess is gay")
                 })
                 
                 if let u = user{
