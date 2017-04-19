f1fa1864dac5a1b52f2cdda71dcd0def7e66b2f0
Blake Howe <blakehowe@hotmail.com>
2017-04-19 23:51:35 +1000

    Login Page - Base
    Implementation of Basic Login Page
    TODO: Logic to authenticate with Firebase...

---
+++ backend-dev/app/app.js
@@ -0,0 +1,12 @@
+(function () {
+	// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAsSZ-RZCDSH2pu2mrsphiCClxoe1X18aA",
+    authDomain: "shuffli-c1b02.firebaseapp.com",
+    databaseURL: "https://shuffli-c1b02.firebaseio.com",
+    projectId: "shuffli-c1b02",
+    storageBucket: "shuffli-c1b02.appspot.com",
+    messagingSenderId: "896912359276"
+  };
+  firebase.initializeApp(config);
+}());
